65e56f8ede1cbc2c58d3e0d939a1ee69e6fdf79d
MIKEWARREN <mikeewarr@icloud.com>
2021-05-10 14:32:17 -0400

    added registration component and route

---
+++ client/src/components/Registration.js
@@ -0,0 +1,45 @@
+
+import {useState} from 'react';
+
+
+function Register (){
+    const [user, setUser] = useState({})
+
+    const newUser = (e) =>{
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const registerUser = () => {
+        fetch('http://localhost:8080/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        }).then(response => response.json())
+        .then(result =>{
+            if (result){
+               console.log('user Registered')
+            }
+        })
+    }
+
+    return (
+
+        <div>
+            <h1>Register</h1>
+            <br />
+            <input type = 'text' onChange={newUser} placeholder='Email' name='email' />
+            <input type = 'text' onChange={newUser} placeholder='Username' name='username' />
+            <input type = 'password' onChange={newUser} placeholder='Password' name='password' />
+            <button onClick={registerUser}>Register</button>
+
+
+        </div>
+    )
+}
+
+export default Register
