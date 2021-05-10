a8b77fafd3a60a1956448363c4b44d71e67c6b25
MIKEWARREN <mikeewarr@icloud.com>
2021-05-10 14:27:11 -0400

    added registration component and route

--- server/app.js
+++ server/app.js
@@ -2,11 +2,41 @@ const express = require('express')
 const cors = require ('cors')
 const models = require('./models')
 const { Op } = require('sequelize')
+var bcrypt = require("bcryptjs");
+
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+app.post('/register', (req,res)=>{
+    let username = req.body.username
+    let password = req.body.password
+    let email = req.body.email
+
+    bcrypt.genSalt(10, function (error,salt){
+        bcrypt.hash(password, salt, function(error, hash){
+            if(!error){
+                let newUser = models.User.build({
+                    username: username,
+                    password: hash,
+                    email: email
+                })
+                newUser.save((error)=>{
+                    if(error){
+                     res.json({ error: "Unable to Register!" });
+                    }else{
+                     res.json({ success: true, message: "Saved new User" });
+                    }
+                })
+            } else {
+             res.json({ success: false });
+
+            }
+        })
+    })  
+})
 
 
 app.post('/app', (req, res) => {
