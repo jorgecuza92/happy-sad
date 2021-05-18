8b32a77f45d6a1ba021d3a2c1e661f50f49d9823
hao <haodam87@gmail.com>
2021-05-18 10:10:10 -0500

    handleclick-fixed

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -15,6 +15,10 @@ class Navbar extends Component {
         this.setState({ clicked: !this.state.clicked })
     }
 
+    handleUnClick = () => {
+        this.setState({ clicked: false })
+    }
+
 
 
 
@@ -30,20 +34,20 @@ class Navbar extends Component {
                 <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
 
                     <li key="Home">
-                        <NavLink to="/" onClick={this.handleClick} className="nav-links">Home</NavLink>
+                        <NavLink to="/" onClick={this.handleUnClick} className="nav-links">Home</NavLink>
                     </li>
                     <li key="Top5">
-                        <NavLink to="/top5" onClick={this.handleClick} className="nav-links">Top 5</NavLink>
+                        <NavLink to="/top5" onClick={this.handleUnClick} className="nav-links">Top 5</NavLink>
                     </li>
                     <li key="Tracker">
-                        <NavLink to="/app-track" onClick={this.handleClick} className="nav-links">Tracker</NavLink>
+                        <NavLink to="/app-track" onClick={this.handleUnClick} className="nav-links">Tracker</NavLink>
                     </li>
                     <li key="Profile">
-                        <NavLink to="/profile" onClick={this.handleClick} className="nav-links">Profile</NavLink>
+                        <NavLink to="/profile" onClick={this.handleUnClick} className="nav-links">Profile</NavLink>
                     </li>
-                    {this.state.clicked ? <div>{this.props.loggedin ? null : <NavLink className="nav-links" onClick={this.handleClick} to="/register">Register</NavLink>}</div> : null}
-                    {this.state.clicked ? <div> {this.props.loggedin ? null :<NavLink className="nav-links" onClick={this.handleClick} to="/login">Login</NavLink>}</div> : null}
-                    {this.state.clicked ? <div>{this.props.loggedin ? <NavLink className="nav-links" onClick={this.handleClick} to="/logout">Logout</NavLink> : null }</div> : null}
+                    {this.state.clicked ? <div>{this.props.loggedin ? null : <NavLink className="nav-links" onClick={this.handleUnClick} to="/register">Register</NavLink>}</div> : null}
+                    {this.state.clicked ? <div> {this.props.loggedin ? null :<NavLink className="nav-links" onClick={this.handleUnClick} to="/login">Login</NavLink>}</div> : null}
+                    {this.state.clicked ? <div>{this.props.loggedin ? <NavLink className="nav-links" onClick={this.handleUnClick} to="/logout">Logout</NavLink> : null }</div> : null}
                 </ul>
                 
                 {this.props.loggedin ? null : <NavLink className="nav-links" to="/register"><Button>Register</Button></NavLink>}
